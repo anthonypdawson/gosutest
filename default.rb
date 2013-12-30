fad4de53b0193fd9e3a95c54d81ce8d3652f3302
Anthony Dawson <anthonypdawson+github@gmail.com>
2013-12-30 11:14:57 -0500

    Added more default values (plus world and player specific), updated to use the new engine classes

--- default.rb
+++ default.rb
@@ -1,6 +1,24 @@
 module Default
+
   Acceleration = 0.5
-  Speed = 4.5
+  TurnSpeed = 4.5
+  Speed = 100
   Angle = 0.0
-  Drag = 0.95
+
+  module World
+    Drag = 0.96 #0.95
+  end
+
+  module Player
+    Acceleration = 0.35
+    TurnSpeed = 4.5
+    Speed = 5
+    Angle = 0.0
+  end
+
+  module Projectile
+    MaxDistance = 300.0
+    Speed = 9.0
+    TTL = 60 * 600.0
+  end
 end
--- game_window.rb
+++ game_window.rb
@@ -1,7 +1,8 @@
 require 'gosu'
-require_relative 'point'
-require_relative 'entity'
-require_relative 'player'
+require_relative 'starship'
+require_relative 'engine/point'
+require_relative 'engine/entity'
+require_relative 'engine/player'
 require_relative 'ZOrder'
 require_relative 'star'
 
@@ -16,7 +17,7 @@ class GameWindow < Gosu::Window
 
     @background_image = Gosu::Image.new(self, "img/Space.png", true)
 
-    @player = Player.new(self)
+    @player = Starship.new(self)
     @player.warp(size.x / 2, size.y / 2)
 
     @star_anim = Gosu::Image::load_tiles(self, "img/Star.png", 25, 25, false)
@@ -35,6 +36,9 @@ class GameWindow < Gosu::Window
     if button_down? Gosu::KbUp or button_down? Gosu::GpButton0 then
       @player.accelerate
     end
+    if button_down? Gosu::KbDown or button_down? Gosu::GpButton1 then
+      @player.deccelerate
+    end
     if button_down? Gosu::KbSpace or button_down? Gosu::Gp3Button2 then
       #@player.shoot
     end
@@ -50,7 +54,7 @@ class GameWindow < Gosu::Window
     @background_image.draw(0,0,ZOrder::Background)
     @player.draw
     @stars.each { |star| star.draw }
-    @font.draw("Score: #{@player.score}", 10, 10, ZOrder::UI, 1.0, 1.0, 0xffffff00)
+    @font.draw("Score: #{@player.score}  #{@player.debug}", 10, 10, ZOrder::UI, 1.0, 1.0, 0xffffff00)
   end
 
   def button_down(id)
